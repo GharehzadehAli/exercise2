ffff9abd6eedaca51508f61daf763054d3ccf583
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-29 13:25:55 +0430

    book class created and implemented.

---
+++ Book.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Book {
+    private String title;
+    private double price;
+
+    public Book(String title) {
+
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
