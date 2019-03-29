3ff42da1fdc7d3f20b2720e05c65e716e9a0d1cd
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-29 13:51:35 +0430

    Fiction.java modified

--- Fiction.java
+++ Fiction.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class Fiction extends Book {
 
+    public Fiction() {
+        setPrice(24.99);
+    }
+
     @Override
     public void setPrice(double price) {
         super.setPrice(price);
-    }
-
-    public Fiction() {
-        setPrice(24.99);
     }
 
 }
